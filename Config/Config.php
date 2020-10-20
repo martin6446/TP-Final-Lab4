71e4208c9603955afb205d9ea256f167af6b0338
BoaglioFranco <francoboaglio@gmail.com>
2020-10-19 23:03:22 -0300

    .

--- Config/Config.php
+++ Config/Config.php
@@ -2,7 +2,7 @@
 
 define("ROOT", dirname(__DIR__) . "/");
 //Path to your project's root folder
-define("FRONT_ROOT", "/Tp Final Lab4/");
+define("FRONT_ROOT", "/TP-Final-Lab4/");
 define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "layout/styles/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
