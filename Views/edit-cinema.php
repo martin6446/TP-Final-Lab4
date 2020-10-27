c62df390c2ac5acacb8d9d29d636f78da54f9139
Martin Caminero <martincaminero64@gmail.com>
2020-10-26 21:46:11 -0300

    some changes that I don't remember

--- Views/edit-cinema.php
+++ Views/edit-cinema.php
@@ -4,37 +4,38 @@
         <div class="form-row">
             <div class="form-group col-md-4">
                 <label for="inputCinemaName">Cinema Name</label>
-                <input type="text" name="name" class="form-control" id="inputCinemaName" required>
+                <input type="text" name="name" class="form-control" id="inputCinemaName" value="<?php echo $cinema->getName()?>" required>
             </div>
             <div class="form-group col-md-4">
                 <label for="inputTicketValue">Ticket Price</label>
-                <input type="number" name="ticketvalue" class="form-control" id="inputTicketValue" required>
+                <input type="number" name="ticketvalue" class="form-control" id="inputTicketValue" value="<?php echo $cinema->getTicketValue()?>" required>
             </div>
             <div class="form-group col-md-4">
                 <label for="inputCapacity">Capacity</label>
-                <input type="number" name="capacity" class="form-control" id="inputCapacity" required>
+                <input type="number" name="capacity" class="form-control" id="inputCapacity" value="<?php echo $cinema->getCapacity()?>" required>
             </div>
         </div>
         <div class="form-row">
         <div class="form-group col-md-6">
             <label for="inputAddress">Address</label>
-            <input type="text" name="address" class="form-control" id="inputAddress" placeholder="1234 Main St" required>
+            <input type="text" name="address" class="form-control" id="inputAddress" value="<?php echo $cinema->getAddress()?>" required>
         </div>
         <div class="form-group col-md-6">
             <label for="inputAddress2">Address 2</label>
-            <input type="text" name="address2" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" required>
+            <input type="number" name="address2" class="form-control" id="inputAddress2" value="<?php echo $cinema->getAddress2()?>" required>
         </div>
         </div>
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="inputCity">City</label>
-                <input type="text" name="city" class="form-control" id="inputCity" required>
+                <input type="text" name="city" class="form-control" id="inputCity" value="<?php echo $cinema->getCity()?>" required>
             </div>
             <div class="form-group col-md-4">
                 <label for="inputState">State</label>
                 <select id="inputState" name="state" class="form-control" required>
                     <option disabled >Choose...</option>
-                    <option selected>Buenos Aires</option>
+                    <option selected ><?php echo "actual state: ". $cinema->getState()?></option>
+                    <option>Buenos Aires</option>
                     <option>Corrientes</option>
                     <option>Salta</option>
                     <option>Tucumán</option>
@@ -42,7 +43,7 @@
             </div>
             <div class="form-group col-md-2">
                 <label for="inputZip">Zip</label>
-                <input type="text" name="zip" class="form-control" id="inputZip" required>
+                <input type="number" name="zip" class="form-control" id="inputZip" value="<?php echo $cinema->getZip()?>" required>
             </div>
         </div>
         <div class="form-group">
