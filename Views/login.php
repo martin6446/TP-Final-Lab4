0ef6bcb6fd867146f4d92f2fce6be8aa0e20ab38
Martin Caminero <martincaminero64@gmail.com>
2020-10-28 16:08:58 -0300

    big update
    	generated a login page and try to use an image for a background
    	but didn't work

--- Views/login.php
+++ Views/login.php
@@ -1,28 +1,36 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
+<nav class="navbar navbar-expand-lg navbar-light bg-dark justify-content-center">
   <figure class="figure">
     <img src="<?php echo IMG_PATH ?>Lumiére3.png" class="figure-img img-fluid rounded" alt="...">
   </figure>
 </nav>
-<div class="container-fluid">
-  <div class="row">
-    <div class="col-sm-6 ">
-      <form action="<?php echo FRONT_ROOT ?>user/userLogin" method="POST" class="border-top border-primary p-3 bg-dark text-white">
-        <div class="form-group">
-          <label for="InputEmail">Email address</label>
-          <input type="email" name="useremail" class="form-control-sm" id="InputEmail" aria-describedby="emailHelp" required>
-          <small id="emailHelp" class="form-text text-muted">Never share your email with anyone else.</small>
+
+<div class="container">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card card-signin my-5">
+          <div class="card-body">
+            <h5 class="card-title text-center">Sign In</h5>
+            <form action="<?php echo FRONT_ROOT?>user/userLogin">
+              <div class="form-label-group">
+                <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address" required autofocus>
+                <label for="inputEmail">Email address</label>
+              </div>
+
+              <div class="form-label-group">
+                <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+                <label for="inputPassword">Password</label>
+              </div>
+
+              <button class="btn btn-md btn-secondary btn-block text-uppercase" name="button" type="submit">Sign in</button>
+
+              <div class="form-label-group">
+                <a href="<?php echo FRONT_ROOT?>user/userRegisterView" class="btn btn-link btn-sm btn-block">Not an User? Sign Up</a>
+              </div>
+              <hr class="my-4">
+              <button class="btn btn-md btn-primary btn-block text-uppercase" type="submit">Sign in with Facebook</button>
+            </form>
+          </div>
         </div>
-        <div class="form-group">
-          <label for="InputPassword">Password</label>
-          <input type="password" name="password" class="form-control-sm" id="InputPassword" required>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
+      </div>
     </div>
   </div>
-
-</div>
-<!-- ver como funciona -->
-<!--   <div class="alert alert-danger" role="alert">
-  A simple danger alert—check it out!
-</div> -->
--- Views/user-register-view.php
+++ Views/user-register-view.php
@@ -1,30 +1,8 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <h1>Lumiére</h1>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Sign In
-                </a>
-                <form action="<?php echo FRONT_ROOT ?>user/userLogin" class="dropdown-menu p-2 dropdown-menu-right">
-                    <div class="form-group ">
-                        <label for="exampleDropdownFormEmail2">Email address</label>
-                        <input type="email" name="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleDropdownFormPassword2">Password</label>
-                        <input type="password" name="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Sign in</button>
-                </form>
-    </div>
-
+<nav class="navbar navbar-expand-lg navbar-light bg-dark justify-content-center">
+  <h1 class="display-4 text-light">Lumiére</h1>
 </nav>
-<h1 class="text-center text-white bg-dark p-1">Registrate</h1>
+
+<!-- <h1 class="text-center text-white bg-dark p-1">Registrate</h1> -->
 <div class="container ">
     <div class="border border-secondary rounded p-1 text-center">
         <form method="POST" action="<?php echo FRONT_ROOT ?>user/userRegister">
