b73de9075dd9f972dba1a61cf943748479b3378c
Martin Caminero <martincaminero64@gmail.com>
2020-11-02 17:21:43 -0300

    fixing stuff

--- Views/footer.php
+++ Views/footer.php
@@ -7,12 +7,7 @@
 </div>
 
 <!-- JAVASCRIPTS -->
-<<<<<<< HEAD
 <!-- <script src="<?php echo JQUERY_PATH?>jquery.min.js"></script>
 <script src="<?php echo JS_PATH?>bootstrap.bundle.min.js"></script> -->
-=======
-<!-- <script src="<?php // echo JQUERY_PATH?>jquery.min.js"></script>
-<script src="<?php// echo JS_PATH?>bootstrap.bundle.min.js"></script> -->
->>>>>>> b62da620ad621fc6ad7c018853ff7d9621f69185
 </body>
 </html>
